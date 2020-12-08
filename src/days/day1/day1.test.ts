c06c3948f4d1f675a73d7c5cb7086244758e51e1
Jeroen Savat <jeroen.savat@lansweeper.com>
2020-12-08 09:51:51 +0100

    initial

---
+++ src/days/day1/day1.test.ts
@@ -0,0 +1,7 @@
+import day1 from "./day1";
+
+describe("App", () => {
+  it("should have some stuff", () => {
+    expect(day1()).toEqual(1);
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import day1 from "./days/day1/day1";
+
+const result = day1();
+
+// eslint-disable-next-line no-console
+console.log(result);
