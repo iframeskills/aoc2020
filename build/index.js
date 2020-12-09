05965a1782ab60a48b039f4efb5932722d71cef0
Jeroen Savat <jeroen.savat@lansweeper.com>
2020-12-09 14:55:15 +0100

    fix: cleanup

--- build/index.js
+++ build/index.js
@@ -3,10 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const day9_1 = require("./days/day9");
-const exercise_1 = __importDefault(require("./days/day9/input/exercise"));
-const result1 = day9_1.part1(exercise_1.default, 25);
-const result2 = day9_1.part2(exercise_1.default, 217430975);
+const day5_1 = __importDefault(require("./days/day5"));
+const exercise_1 = __importDefault(require("./days/day5/input/exercise"));
+const result = day5_1.default(exercise_1.default);
 // eslint-disable-next-line no-console
-console.log(result1, result2);
+console.log(result);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { part1, part2 } from "./days/day9";
-import input from "./days/day9/input/exercise";
+import day5 from "./days/day5";
+import input from "./days/day5/input/exercise";
 
-const result1 = part1(input, 25);
-const result2 = part2(input, 217430975);
+const result = day5(input);
 
 // eslint-disable-next-line no-console
-console.log(result1, result2);
+console.log(result);
