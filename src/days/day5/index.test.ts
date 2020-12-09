cda1134eeabaf4b0fb1fee547777d351392e2310
Jeroen Savat <jeroen.savat@lansweeper.com>
2020-12-09 15:49:15 +0100

    feature: day5

--- src/days/day5/index.test.ts
+++ src/days/day5/index.test.ts
@@ -1,8 +1,44 @@
-import day from ".";
+import day, {
+  getRow,
+  getColumn,
+  getId,
+  getMissingFromList,
+  decodeTicket,
+} from ".";
 import sampleinput from "./input/sample";
 import sampleoutput from "./output/sample";
 
+import exerciseinput from "./input/exercise";
+import part1output from "./output/part1";
+import part2output from "./output/part2";
+
 describe("solution", () => {
+  it("getRow should return", () => {
+    const input = "FBFBBFF";
+    const result = 44;
+    expect(getRow(input)).toEqual(result);
+  });
+
+  it("getColumn should return", () => {
+    const input = "RLR";
+    const result = 5;
+    expect(getColumn(input)).toEqual(result);
+  });
+
+  it("getId should return", () => {
+    expect(getId(44, 5)).toEqual(357);
+  });
+
+  it("decodeTicket should return", () => {
+    const input = "FBFBBFFRLR";
+    const result = { id: 357, row: 44, column: 5 };
+    expect(decodeTicket(input)).toEqual(result);
+  });
+
+  it("getMissingFromList should return", () => {
+    expect(getMissingFromList([1, 2, 3, 5, 6])).toEqual(4);
+  });
+
   it("sample input should match output", () => {
     /*
 
@@ -14,9 +50,12 @@ describe("solution", () => {
     */
     expect(day(sampleinput).resultpart1).toEqual(sampleoutput);
   });
-  /*
+
   it("PART 1 - sample input should match output", () => {
-    expect(day(sampleinput).resultpart1).toEqual(sampleoutput);
+    expect(day(exerciseinput).resultpart1).toEqual(part1output);
   });
-  */
+
+  it("PART 2 - sample input should match output", () => {
+    expect(day(exerciseinput).resultpart2).toEqual(part2output);
+  });
 });
--- src/days/day5/index.ts
+++ src/days/day5/index.ts
@@ -1,4 +1,53 @@
+export const getPosition = (
+  input: string,
+  maximum: number,
+  lowerboundCharacter: string,
+  upperboundCharacter: string
+) => {
+  const directions = input.split("");
+  const range = Array.from({ length: maximum }, (_, index) => index + 1);
+
+  directions.forEach((direction) => {
+    if (direction === lowerboundCharacter) {
+      range.splice(0, range.length / 2);
+    }
+    if (direction === upperboundCharacter) {
+      range.splice(range.length / 2, range.length);
+    }
+  });
+
+  // make it 0-based; -1
+  return range[0] - 1;
+};
+
+export const getRow = (input: string): number =>
+  getPosition(input, 128, "B", "F");
+export const getColumn = (input: string): number =>
+  getPosition(input, 8, "R", "L");
+export const getId = (row: number, column: number) => row * 8 + column;
+
+export const getMissingFromList = (list: number[]) => {
+  let missingEntry: number;
+
+  list.forEach((number, index) => {
+    const nextValue = list[index + 1];
+    const nextExpectedValue = number + 1;
+    if (nextValue && nextValue !== nextExpectedValue) {
+      missingEntry = nextExpectedValue;
+    }
+  });
+
+  return missingEntry;
+};
+
+export const decodeTicket = (input: string) => ({
+  id: getId(getRow(input), getColumn(input)),
+  row: getRow(input),
+  column: getColumn(input),
+});
+
 export default (input: string) => {
+  const lines: string[] = input.split("\n");
   // F means "front", B means "back", L means "left", and R means "right".
 
   // First 7 characters = row "dividers" for range 0-127
@@ -8,11 +57,17 @@ export default (input: string) => {
 
   // What is the highest seat ID on a boarding pass?
 
-  // { id: 357, row: 44, column: 5 };
+  const tickets = lines.map((line) => decodeTicket(line));
+  const ticketIds = tickets.map((ticket) => ticket.id);
 
-  const resultpart1 = 820;
+  const highestSeatId = Math.max(...ticketIds);
 
-  const resultpart2 = "";
+  const resultpart1 = highestSeatId;
+
+  // what id is missing, (but not at the start or end)
+  const sortedTicketids = ticketIds.sort((a, b) => a - b);
+
+  const resultpart2 = getMissingFromList(sortedTicketids);
 
   return { resultpart1, resultpart2 };
 };
